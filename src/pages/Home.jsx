b45fc2e7499db01682c6de8a0899b3a336aab1d1
Victoria Walsh-Herranz <victoriawh@Victorias-MacBook-Air.local>
2025-08-08 19:06:19 +0100

    added theme toggle basic functionality

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
+import { ThemeToggle } from "../components/ThemeToggle"
+
 export const Home = () => {
 return <div className="min-h-screen ">
 
 {/* Theme toggle */}
-
+< ThemeToggle/>
 {/* Background Effects */}
 
 {/* Navbar */}
