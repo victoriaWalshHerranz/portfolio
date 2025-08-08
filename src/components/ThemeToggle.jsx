1f7f21786812f50a66b8632dd315259d184f48f8
Victoria Walsh-Herranz <victoriawh@Victorias-MacBook-Air.local>
2025-08-08 19:18:00 +0100

    created util for twmerge

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,5 +1,6 @@
 import { Moon, Sun } from "lucide-react";
 import { useEffect, useState } from "react"
+import { cn } from "../lib/utils";
 
 export const ThemeToggle = () => {
 const [isDarkMode, setIsDarkMode] = useState(false);
@@ -28,7 +29,13 @@ const toggleTheme = () => {
 
 
     return (
-    <button onClick={toggleTheme}>
+    <button 
+    onClick={toggleTheme}
+    className={cn(
+        "fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300",
+        "focus:outline-hidden"
+    )}
+    >
         {""}
         {isDarkMode ? (
          <Sun className="size-6 text-yellow-300"/> 
