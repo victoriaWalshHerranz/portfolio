4379e7a53575ebd59d3f0a8d4f22d417c44785da
Victoria Walsh-Herranz <victoriawh@Victorias-MacBook-Air.local>
2025-09-05 10:18:59 +0100

    added background to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,18 @@
-import { ThemeToggle } from "../components/ThemeToggle"
+import { ThemeToggle } from "../components/ThemeToggle";
+import { StarBackground } from "../components/StarBackground";
 
 export const Home = () => {
-return <div className="min-h-screen ">
+  return (
+    <div className="min-h-screen ">
+      {/* Theme toggle */}
+      <ThemeToggle />
+      {/* Background Effects */}
+      <StarBackground />
+      {/* Navbar */}
 
-{/* Theme toggle */}
-< ThemeToggle/>
-{/* Background Effects */}
+      {/* Main Content */}
 
-{/* Navbar */}
-
-{/* Main Content */}
-
-{/* Footer */}
-</div>
-}
+      {/* Footer */}
+    </div>
+  );
+};
