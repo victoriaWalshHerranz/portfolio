f84f82399ba460bc0a3e384e175da079a4baf76e
Victoria Walsh-Herranz <victoriawh@Victorias-MacBook-Air.local>
2025-09-05 20:14:10 +0100

    added skills section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import { StarBackground } from "../components/StarBackground";
 import { Navbar } from "../components/Navbar";
 import { HeroSection } from "../components/HeroSection";
 import { AboutSection } from "../components/AboutSection";
+import { SkillsSection } from "../components/SkillsSection";
 
 export const Home = () => {
   return (
@@ -17,6 +18,7 @@ export const Home = () => {
       <main>
         <HeroSection />
         <AboutSection />
+        <SkillsSection />
       </main>
       {/* Footer */}
     </div>
