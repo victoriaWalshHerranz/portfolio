0fa5217ffb829c99a472af4e3f96b13e392de484
Victoria Walsh-Herranz <victoriawh@Victorias-MacBook-Air.local>
2025-09-05 15:24:33 +0100

    added hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { ThemeToggle } from "../components/ThemeToggle";
 import { StarBackground } from "../components/StarBackground";
 import { Navbar } from "../components/Navbar";
+import { HeroSection } from "../components/HeroSection";
 
 export const Home = () => {
   return (
@@ -12,7 +13,9 @@ export const Home = () => {
       {/* Navbar */}
       <Navbar />
       {/* Main Content */}
-
+      <main>
+        <HeroSection />
+      </main>
       {/* Footer */}
     </div>
   );
